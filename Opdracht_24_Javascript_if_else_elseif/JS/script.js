daad091eb094f07b47d79c5fe1b7d2d1be816231
DFW2025 <81567386+DFW2025@users.noreply.github.com>
2021-05-31 16:45:44 +0200

    Add files via upload

---
+++ Opdracht_24_Javascript_if_else_elseif/JS/script.js
@@ -0,0 +1,17 @@
+// Javascript
+
+const age= 17;
+
+if (age == 18) { console.log("you are just 18, welcome!!"); }
+else if (age > 18) { console.log("you are above 18, welcome!!"); }
+else { console.log("you are under 18, please come back when you're 18 or older!!"); }
+
+const isFemale = false;
+
+if (isFemale) { console.log("thanks for confirming you're a lady, welcome to the club!!"); }
+else { console.log("sorry this is just for the ladies, no men are allowed!!"); }
+
+const driverStatus = "nobob";
+
+if (driverStatus =="bob") { console.log("you are the BOB for tonight and are allowed to drive safely to home!!"); }
+else { console.log("sorry you're not the BOB tonight and aren't allowed to drive!!"); }
